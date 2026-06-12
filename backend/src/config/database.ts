0f6da2c70c41154c7ea28667a251192dfa7d23fb
leticia-csilva <leticias.2023@alunos.utfpr.edu.br>
2026-06-12 08:09:24 -0300

    feat: registro de logs

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import { Usuario } from "../entities/Usuario";
 import { Avaliacao } from "../entities/Avaliacao";
 import { AvaliacaoAnexo } from "../entities/AvaliacaoAnexo";
+import { LogSistema } from "../entities/LogSistema";
 import { Paciente } from "../entities/Paciente";
 
 dotenv.config();
@@ -15,7 +16,7 @@ export const AppDataSource = new DataSource({
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  entities: [Usuario, Avaliacao, AvaliacaoAnexo, Paciente],
+  entities: [Usuario, Avaliacao, AvaliacaoAnexo, Paciente, LogSistema],
   synchronize: true,
   logging: false
 });
